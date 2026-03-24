d241250f19fae6f40d7d3d392d0b588840ca81f6
pranav100000 <13628515+pranav100000@users.noreply.github.com>
2026-03-24 08:34:13 +0000

    Update from Aether

--- src/instrument.js
+++ src/instrument.js
@@ -1,6 +1,7 @@
 const Sentry = require("@sentry/node");
+const config = require("./config");
 
 Sentry.init({
-  dsn: "https://df3678b7bd8489203bc70fc932bdbb22@o4510703250505728.ingest.us.sentry.io/4510854705053696",
+  dsn: config.sentryDsn,
   sendDefaultPii: true,
 });
